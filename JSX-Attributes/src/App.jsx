245db4acaead0c67b81270c7a80448cf20233f0a
GAURAV SHARMA <gsharma1154@gmail.com>
2024-01-19 17:37:38 +0530

    inline css and styling

--- JSX-Attributes/src/App.jsx
+++ JSX-Attributes/src/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import './App.css'
+
+
+const name11={
+  marginTop: '50px',
+  textAlign: 'center',
+  fontFamily: "'Noto Sans TC', sans-serif"
+}
+
+
+
 const App = () => {
 
   const name='Gaurav';
@@ -13,7 +23,7 @@ const App = () => {
   const link='https://telusko.com/';
   return (
     <>
-      <h1 className='name'>My name is:{name} and I am learning React </h1>
+      <h1 style={name11}>My name is:{name} and I am learning React </h1>
       <div className='he'>
 
       <img src={image1} alt="Image not found" className='k1' />
