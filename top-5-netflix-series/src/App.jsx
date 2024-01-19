4808be9c16f6b6c835ce24affd1d1ec1ff801a9a
GAURAV SHARMA <gsharma1154@gmail.com>
2024-01-19 14:00:21 +0530

    made a static webSite top 5 movie website

---
+++ top-5-netflix-series/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+<h1 className='ai'>Gaurav technical Netflix pick</h1>
+<p>
+List of 5 best Netflix series:
+<ol>
+<li>Dark</li>
+<li>Breaking Bad</li>
+<li>Narcos</li>
+<li>Bhag Milkha Bhag</li>
+<li>Amercian Psycho</li>
+</ol>
+
+</p>
+
+    </>
+  )
+}
+
+export default App
