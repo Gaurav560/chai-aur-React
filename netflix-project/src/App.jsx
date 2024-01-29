7b4bd54a620cb1d5c59f64d33f075f4f514aa214
Gaurav Sharma <gsharma1154@gmail.com>
2024-01-29 12:13:00 +0530

    used array to reduce no of lines in code

--- netflix-project/src/App.jsx
+++ netflix-project/src/App.jsx
@@ -2,39 +2,45 @@ import React from 'react'
 import './Card'
 import Card from './Card.jsx'
 import './App.css'
+import Sdata from './Sdata.jsx'
+
+
+console.log(Sdata[0]);
+
 const App = () => {
   return (
     <>
     <h1 className='h11'>Netflix's Best</h1>
-<Card img_src="https://wallpaperset.com/w/full/4/1/c/40991.jpg"
-  title="A Neflix Original Series"
-  sname="Narcos"
-  link="https://www.netflix.com/in/title/80025172"
+<Card img_src={Sdata[0].img_src}
+  title={Sdata[0].title}
+  sname={Sdata[0].sname}
+  link={Sdata[0].link}
 />
-<Card img_src="https://rb.gy/kepvlx"
-  title="A Neflix Original Series"
-  sname="Breaking Bad"
-  link="https://www.netflix.com/in/title/70143836"
-/>
-<Card img_src="https://rb.gy/3jgqgq"
-  title="A Neflix Original Series"
-  sname="Better Call Saul"
-  link="https://www.netflix.com/in/title/80021955"
+<Card img_src={Sdata[1].img_src}
+  title={Sdata[1].title}
+  sname={Sdata[1].sname}
+  link={Sdata[1].link}
+
 />
 
-<Card img_src="https://images.immediate.co.uk/production/volatile/sites/3/2019/09/18563804-low_res-peaky-blinders-v-9255587.jpg?quality=90&webp=true&resize=1500,1001"
-  title="A Neflix Original Series"
-  sname="Peaky Blinders"
-  link="https://www.netflix.com/in/title/80002479"
+<Card img_src={Sdata[2].img_src}
+  title={Sdata[2].title}
+  sname={Sdata[2].sname}
+  link={Sdata[2].link}
 />
 
-
-
-<Card img_src="https://wallpaperset.com/w/full/9/b/f/33895.jpg"
-  title="A Neflix Original Series"
-  sname="GoodFellas"
-  link="https://www.netflix.com/be-en/title/70002022"
+<Card img_src={Sdata[3].img_src}
+  title={Sdata[3].title}
+  sname={Sdata[3].sname}
+  link={Sdata[3].link}
 />
+
+<Card img_src={Sdata[4].img_src}
+  title={Sdata[4].title}
+  sname={Sdata[4].sname}
+  link={Sdata[4].link}
+/>
+
     </>
   )
 }
---
+++ netflix-project/src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata=[
+    {
+    img_src:"https://wallpaperset.com/w/full/4/1/c/40991.jpg",
+    title:"A Neflix Original Series",
+    sname:"Narcos",
+    link:"https://www.netflix.com/in/title/80025172"
+    },
+    {
+    img_src:"https://rb.gy/kepvlx",
+    title:"A Neflix Original Series",
+    sname:"Breaking Bad",
+    link:"https://www.netflix.com/in/title/70143836"
+        
+    },
+        
+       
+    {
+    img_src:"https://rb.gy/3jgqgq",
+    title:"A Neflix Original Series",
+    sname:"Better Call Saul",
+    link:"https://www.netflix.com/in/title/80021955"
+        
+    }, 
+            
+    {
+    img_src:"https://images.immediate.co.uk/production/volatile/sites/3/2019/09/18563804-low_res-peaky-blinders-v-9255587.jpg?quality=90&webp=true&resize=1500,1001",
+    title:"A Neflix Original Series",
+    sname:"Peaky Blinders",
+    link:"https://www.netflix.com/in/title/80002479"
+    },
+
+    {
+    img_src:"https://wallpaperset.com/w/full/9/b/f/33895.jpg",
+    title:"A Neflix Original Series",
+    sname:"GoodFellas",
+    link:"https://www.netflix.com/be-en/title/70002022"
+     }
+];
+
+export default Sdata;
