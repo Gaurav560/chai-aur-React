2635685e53a025cd604ef20ddd00c782f8533ad5
Gaurav Sharma <gsharma1154@gmail.com>
2024-01-29 12:37:51 +0530

    using map and fat arrow fn to reduce the no of lines

--- netflix-project/src/App.jsx
+++ netflix-project/src/App.jsx
@@ -1,48 +1,22 @@
-import React from 'react'
-import './Card'
-import Card from './Card.jsx'
-import './App.css'
-import Sdata from './Sdata.jsx'
-
-
-console.log(Sdata[0]);
+import React from 'react';
+import Card from './Card';
+import './App.css';
+import Sdata from './Sdata';
 
 const App = () => {
   return (
     <>
-    <h1 className='h11'>Netflix's Best</h1>
-<Card img_src={Sdata[0].img_src}
-  title={Sdata[0].title}
-  sname={Sdata[0].sname}
-  link={Sdata[0].link}
-/>
-<Card img_src={Sdata[1].img_src}
-  title={Sdata[1].title}
-  sname={Sdata[1].sname}
-  link={Sdata[1].link}
-
-/>
-
-<Card img_src={Sdata[2].img_src}
-  title={Sdata[2].title}
-  sname={Sdata[2].sname}
-  link={Sdata[2].link}
-/>
-
-<Card img_src={Sdata[3].img_src}
-  title={Sdata[3].title}
-  sname={Sdata[3].sname}
-  link={Sdata[3].link}
-/>
-
-<Card img_src={Sdata[4].img_src}
-  title={Sdata[4].title}
-  sname={Sdata[4].sname}
-  link={Sdata[4].link}
-/>
-
+      <h1 className='h11'>Netflix's Best</h1>
+      {Sdata.map((val) => (
+        <Card 
+          img_src={val.img_src}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      ))}
     </>
   )
 }
 
-export default App
+export default App;
