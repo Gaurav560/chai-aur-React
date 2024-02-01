04a908c96d5bec817f53c134988d8da6e3ec1baa
GAURAV SHARMA <gsharma1154@gmail.com>
2024-02-01 08:46:42 +0530

    slot machine with props passing to children and conditional rendering and emojisense extension

---
+++ slot-machine/src/SlotM.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import True from './True';
+import False from './False';
+
+const SlotM = (props) => {
+  let { x, y, z } = props;
+
+  if (x === y && y === z) {
+    return <True x={x} y={y} z={z} />;
+  } else {
+    return <False x={x} y={y} z={z} />;
+  }
+};
+
+export default SlotM;
