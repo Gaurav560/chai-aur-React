ba37f929b8c71e12d0b0c341511eb63972ce8d20
Gaurav Sharma <gsharma1154@gmail.com>
2024-02-29 23:04:41 +0530

    react forms

---
+++ react-forms/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<>
+<div>
+
+  <h1>Hello</h1>
+  <input type="text" placeholder='Enter your Name' />
+  <button>Submit</button>
+</div>
+</>
+  )
+}
+
+export default App
