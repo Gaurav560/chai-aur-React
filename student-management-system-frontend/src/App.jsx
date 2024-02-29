00639974a9d094a768845e644de89d5f03c60d3b
Gaurav Sharma <gsharma1154@gmail.com>
2024-03-01 01:20:55 +0530

    student frontend done

--- student-management-system-frontend/src/App.jsx
+++ student-management-system-frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StudentList from './StudentList';
+import AddStudent from './AddStudent';
+import EditStudent from './EditStudent';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1>Telusko Student Management System</h1>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<StudentList />} exact />
+          <Route path="/add" element={<AddStudent />} />
+          <Route path="/edit/:id" element={<EditStudent />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
