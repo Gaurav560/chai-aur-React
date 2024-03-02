833cdb907b7f63c3f9c4e2adf6db1d4394b0ca01
Gaurav Sharma <gsharma1154@gmail.com>
2024-03-02 12:27:50 +0530

    completed react-forms

--- react-forms/src/App.jsx
+++ react-forms/src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [name,setName]=useState('');
+  const[fullName,setFullName]=useState();
+
+const inputEvent=(event)=>{
+  setName(event.target.value)
+
+}
+const onSubmit=()=>{
+setFullName(name);
+}
+
+
+
   return (
 <>
 <div>
-
-  <h1>Hello</h1>
-  <input type="text" placeholder='Enter your Name' />
-  <button>Submit</button>
+  <h1>Hello {fullName}</h1>
+  <input type="text" placeholder='Enter your Name' onChange={inputEvent} value={name}/>
+  <button onClick={onSubmit}>Click Me</button>
 </div>
 </>
   )
