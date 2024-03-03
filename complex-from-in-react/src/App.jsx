a0f6f04e1f4b7e79b955d47a8b27ad46ad5895b7
GAURAV SHARMA <gsharma1154@gmail.com>
2024-03-03 11:46:57 +0530

    completed complex form

--- complex-from-in-react/src/App.jsx
+++ complex-from-in-react/src/App.jsx
@@ -1,26 +1,61 @@
 import React, { useState } from 'react'
 
 const App = () => {
+  const [student,setStudent]=useState({
+    fName:'',
+    lName:'',
+    email:'',
+    pNumber:'',
+  });
 
-  const [name,setName]=useState('');
-  const [lastName,setLastName]=useState('');
-  const[fullName,setFullName]=useState();
-  const[lastFullName,setLastFullName]=useState();
 
 const inputEvent=(event)=>{
-  setName(event.target.value)
+// const value=event.target.value;
+// const name=event.target.name;
 
+//doing the same with the help of array destructuring
+const{value, name}=event.target;
+
+
+
+setStudent((prevValue)=>{
+  if(name==='fName'){
+    return{
+      fName:value,
+      lName:prevValue.lName,
+      email:prevValue.email,
+      pNumber:prevValue.pNumber
+    };
+  }else if(name==='lName'){
+    return{
+      fName:prevValue.fName,
+      lName:value,
+      email:prevValue.email,
+      pNumber:prevValue.pNumber
+
+    };
+  }else if(name==='email'){
+    return{
+      fName:prevValue.fName,
+      lName:prevValue.lName,
+      email:value,
+      pNumber:prevValue.pNumber
+    };
+  }else{
+    return{
+      fName:prevValue.fName,
+      lName:prevValue.lName,
+      email:prevValue.email,
+      pNumber:value
+    };
+  }
+})
 }
 
 
-const inputEventTwo=(event)=>{
-  setLastName(event.target.value)
-
-}
 const onSubmits=(event)=>{
   event.preventDefault();
-setFullName(name);
-setLastFullName(lastName);
+  alert('form submitted');
 }
 
 
@@ -30,9 +65,13 @@ setLastFullName(lastName);
 <div>
 <form onSubmit={onSubmits}>
 <div>
-  <h1>Hello {fullName} {lastFullName}</h1>
-  <input type="text" placeholder='Enter your FirstName' onChange={inputEvent} value={name}/>
-  <input type="text" placeholder='Enter your LastName' onChange={inputEventTwo} value={lastName}/>
+  <h1>Hello {student.fName} {student.lName} {student.email} {student.pNumber}</h1>
+  <input type="text" placeholder='Enter your FirstName' name='fName' onChange={inputEvent} 
+  //value={student.fName}
+  />
+  <input type="text" placeholder='Enter your LastName'name='lName' onChange={inputEvent} value={student.lName}/>
+  <input type="email" placeholder='Enter your Email'name='email' onChange={inputEvent} value={student.email}/>
+  <input type="number" placeholder='Enter your PhoneNumber'name='pNumber' onChange={inputEvent} value={student.pNumber}/>
   <button type='submit'>Click Me</button>
 </div>
 </form>
