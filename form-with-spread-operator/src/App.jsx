0cb0883168dfbad9c926809bc9f24e2b1ace2c72
GAURAV SHARMA <gsharma1154@gmail.com>
2024-03-04 11:54:02 +0530

    using spread operator

--- form-with-spread-operator/src/App.jsx
+++ form-with-spread-operator/src/App.jsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [student,setStudent]=useState({
+    fName:'',
+    lName:'',
+    email:'',
+    pNumber:'',
+  });
+
+
+const inputEvent=(event)=>{
+// const value=event.target.value;
+// const name=event.target.name;
+
+//doing the same with the help of array destructuring
+const{value, name}=event.target;
+
+
+
+setStudent((prevValue)=>{
+  if(name==='fName'){
+    return{
+      fName:value,
+      lName:prevValue.lName,
+      email:prevValue.email,
+      pNumber:prevValue.pNumber
+    };
+  }else if(name==='lName'){
+    return{
+      fName:prevValue.fName,
+      lName:value,
+      email:prevValue.email,
+      pNumber:prevValue.pNumber
+
+    };
+  }else if(name==='email'){
+    return{
+      fName:prevValue.fName,
+      lName:prevValue.lName,
+      email:value,
+      pNumber:prevValue.pNumber
+    };
+  }else{
+    return{
+      fName:prevValue.fName,
+      lName:prevValue.lName,
+      email:prevValue.email,
+      pNumber:value
+    };
+  }
+})
+}
+
+
+const onSubmits=(event)=>{
+  event.preventDefault();
+  alert('form submitted');
+}
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<>
+<div>
+<form onSubmit={onSubmits}>
+<div>
+  <h1>Hello {student.fName} {student.lName} {student.email} {student.pNumber}</h1>
+  <input type="text" placeholder='Enter your FirstName' name='fName' onChange={inputEvent} 
+  //value={student.fName}
+  />
+  <input type="text" placeholder='Enter your LastName'name='lName' onChange={inputEvent} value={student.lName}/>
+  <input type="email" placeholder='Enter your Email'name='email' onChange={inputEvent} value={student.email}/>
+  <input type="number" placeholder='Enter your PhoneNumber'name='pNumber' onChange={inputEvent} value={student.pNumber}/>
+  <button type='submit'>Click Me</button>
+</div>
+</form>
+</div>
+</>
   )
 }
 
